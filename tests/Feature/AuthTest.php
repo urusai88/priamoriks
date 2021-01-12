4c0336efc19192403090ad421f03b5dddd155e11
urusai88 <archon.saratov@gmail.com>
2021-01-13 01:24:53 +0400

    fix AuthTest.php

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,45 +4,32 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\AppTestHelpers;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
     use DatabaseMigrations;
     use RefreshDatabase;
-
-    protected function signup($name)
-    {
-        return $this->postJson('/api/signup', ['name' => $name])->json('api_token');
-    }
-
-    public function testSignup()
-    {
-        $this->postJson('/api/signup')->assertStatus(422);
-        $this->postJson('/api/signup', ['name' => ''])->assertStatus(422);
-        $this->postJson('/api/signup', ['name' => 'name 1'])->assertJsonStructure(['api_token']);
-        $this->postJson('/api/signup', ['name' => 'name 2'])->assertJsonStructure(['api_token']);
-        $this->postJson('/api/signup', ['name' => 'name 2'])->assertStatus(422);
-
-        $this->assertDatabaseHas('users', ['name' => 'name 1']);
-        $this->assertDatabaseHas('users', ['name' => 'name 2']);
-    }
+    use AppTestHelpers;
 
     public function testSignin()
     {
-        $this->signup('name 1');
-
+        // empty request
         $this->postJson('/api/signin')->assertStatus(422);
-        $this->postJson('/api/signin', ['name' => 'name 2'])->assertStatus(422);
+        // success
         $apiToken = $this->postJson('/api/signin', ['name' => 'name 1'])
             ->assertSuccessful()
             ->assertJsonStructure(['api_token'])
             ->json('api_token');
 
+        // no token
         $this->getJson('/api/user')->assertStatus(401);
+        // wrong token
         $this->getJson('/api/user', [
             'authorization' => "Bearer unknown",
         ])->assertStatus(401);
+        // good token
         $this->getJson('/api/user', [
             'authorization' => "Bearer $apiToken",
         ])->assertSuccessful()->assertJson(['name' => 'name 1']);
