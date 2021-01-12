a97f9513bcb55511352be7c3090588200a638950
urusai88 <archon.saratov@gmail.com>
2021-01-12 21:26:48 +0400

    cors middleware fix users api_token migration users seeder signup page create sqlite db console command

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('api_token', 16);
+            $table->string('api_token', 16)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
